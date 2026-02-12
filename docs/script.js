c65d9c9133ba2bf94ae946e50430cdbc330611e6
MAXWEL MXL <maxwelthuo56@gmail.com>
2026-02-12 20:27:48 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,13 @@
+console.log("JS is working");
+
 console.log("Script Loaded");
 
 window.addEventListener("DOMContentLoaded", function () {
     loadUser();
     loadQuiz();
 });
-function loadQuiz() {
+function loadQuiz() 
+
     if (!document.getElementById("question")) return;
     showQuestion();
 }
