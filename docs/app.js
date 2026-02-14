70d359b62d609178531f80e7195b02eb24fb7aaf
MAXWEL MXL <maxwelthuo56@gmail.com>
2026-02-14 19:14:58 +0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,36 @@
 const user = localStorage.getItem("loggedInUser");
 
-if(!user){
+if(!user && !window.location.href.includes("index.html")){
     window.location.href = "index.html";
 }
 
-function openModule4(){
-    window.location.href = "module4.html";
+function logout(){
+    localStorage.removeItem("loggedInUser");
+    window.location.href = "index.html";
+}
+
+function completeModule(moduleNumber){
+    localStorage.setItem("module" + moduleNumber, "completed");
+    updateProgress();
+}
+
+function updateProgress(){
+    let completed = 0;
+
+    for(let i=1; i<=4; i++){
+        if(localStorage.getItem("module"+i) === "completed"){
+            completed++;
+        }
+    }
+
+    let percent = (completed/4)*100;
+
+    const bar = document.getElementById("progressFill");
+    if(bar){
+        bar.style.width = percent + "%";
+    }
+
+    if(percent === 100){
+        localStorage.setItem("certificate","unlocked");
+    }
 }
